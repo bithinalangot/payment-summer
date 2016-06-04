141ff32c29141d074d8ab9b901fba11c41f8315e
tonythomas01 <01tonythomas@gmail.com>
2016-06-04 15:27:19 +0530

    update failure and success pages

--- failure.php
+++ failure.php
@@ -1,36 +1,92 @@
-<?php
-$status=$_POST["status"];
-$firstname=$_POST["firstname"];
-$amount=$_POST["amount"];
-$txnid=$_POST["txnid"];
+<!DOCTYPE html>
+<html lang="en">
 
-$posted_hash=$_POST["hash"];
-$key=$_POST["key"];
-$productinfo=$_POST["productinfo"];
-$email=$_POST["email"];
-$salt="GQs7yium";
+<head>
+	<meta charset="utf-8">
+	<title>The Annual Conference</title>
 
-If (isset($_POST["additionalCharges"])) {
-       $additionalCharges=$_POST["additionalCharges"];
-        $retHashSeq = $additionalCharges.'|'.$salt.'|'.$status.'|||||||||||'.$email.'|'.$firstname.'|'.$productinfo.'|'.$amount.'|'.$txnid.'|'.$key;
-        
-                  }
-	else {	  
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
 
-        $retHashSeq = $salt.'|'.$status.'|||||||||||'.$email.'|'.$firstname.'|'.$productinfo.'|'.$amount.'|'.$txnid.'|'.$key;
+	<link href="font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="scripts/jquery-ui-1.11.2/jquery-ui.min.css" rel="stylesheet" />
+	<link href="animate.min.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
 
-         }
-		 $hash = hash("sha512", $retHashSeq);
-  
-       if ($hash != $posted_hash) {
-	       echo "Invalid Transaction. Please try again";
-		   }
-	   else {
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+	<script>
+		function initialize() {
+			var myLatlng = new google.maps.LatLng(12.894655,77.675153);
+			var map_options = {
+				zoom: 16,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				center: myLatlng,
+				scrollwheel: false,
+				disableDefaultUI: true
+			}
 
-         echo "<h3>Your order status is ". $status .".</h3>";
-         echo "<h4>Your transaction id for this transaction is ".$txnid.". You may try making the payment by clicking the link below.</h4>";
-          
-		 } 
-?>
-<!--Please enter your website homepagge URL -->
-<p><a href=http://localhost/testing/success_failure/PayUMoney_form.php> Try Again</a></p>
+			var map = new google.maps.Map(document.getElementById('map_canvas'), map_options)
+			var myIcon = new google.maps.MarkerImage('images/map_icon.png', null, null, null, new google.maps.Size(36,54));
+
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				icon: myIcon
+			});
+
+			/* Remove if you dont want B/W Google Map */
+			/*
+			 var styles = [
+			 {
+			 featureType: "all",
+			 stylers: [
+			 { saturation: -100 }
+			 ]
+			 }
+			 ];
+
+			 map.setOptions({styles: styles});
+			 */
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
+	<!-- Google Map Ends! -->
+
+</head>
+
+<body>
+
+<div class="registration" style="padding-top:10%">
+	<div class="container">
+
+		<h2>Conference Registration</h2>
+		<p class="subtitle"><strong>Something went wrong :( </strong> Please try again!</p>
+
+	</div>
+	<!-- registration ends! -->
+
+	<!-- footer -->
+	<div class="footer" style=" bottom: 0px; position: fixed; width: 100%;">
+		<div class="container" style="text-align: center">
+
+			<p style="text-align: center">Copyright 2016 <a href="http://www.amrita.edu">Amrita Center for Cybersecurity Systems and Networks</a></p>
+
+		</div>
+	</div>
+	<!-- footer ends! -->
+
+
+	<!-- scripts -->
+	<script src="scripts/jquery-1.11.0.min.js"></script>
+	<script src="scripts/jquery-ui-1.11.2/jquery-ui.min.js"></script>
+	<script src="scripts/flexslider/jquery.flexslider-min.js"></script>
+	<script src="scripts/jquery.parallax-1.1.3.js"></script>
+	<script src="scripts/jquery.inview.min.js"></script>
+	<script src="scripts/form.js"></script>
+	<script src="scripts/theme.js"></script>
+	<!-- scripts ends! -->
+</body>
+</html>
--- success.php
+++ success.php
@@ -1,34 +1,98 @@
-<?php
-$status=$_POST["status"];
-$firstname=$_POST["firstname"];
-$amount=$_POST["amount"];
-$txnid=$_POST["txnid"];
-$posted_hash=$_POST["hash"];
-$key=$_POST["key"];
-$productinfo=$_POST["productinfo"];
-$email=$_POST["email"];
-$salt="GQs7yium";
+<!DOCTYPE html>
+<html lang="en">
 
-If (isset($_POST["additionalCharges"])) {
-       $additionalCharges=$_POST["additionalCharges"];
-        $retHashSeq = $additionalCharges.'|'.$salt.'|'.$status.'|||||||||||'.$email.'|'.$firstname.'|'.$productinfo.'|'.$amount.'|'.$txnid.'|'.$key;
-        
-                  }
-	else {	  
+<head>
+	<meta charset="utf-8">
+	<title>The Annual Conference</title>
 
-        $retHashSeq = $salt.'|'.$status.'|||||||||||'.$email.'|'.$firstname.'|'.$productinfo.'|'.$amount.'|'.$txnid.'|'.$key;
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
 
-         }
-		 $hash = hash("sha512", $retHashSeq);
-		 
-       if ($hash != $posted_hash) {
-	       echo "Invalid Transaction. Please try again";
-		   }
-	   else {
-           	   
-          echo "<h3>Thank You. Your order status is ". $status .".</h3>";
-          echo "<h4>Your Transaction ID for this transaction is ".$txnid.".</h4>";
-          echo "<h4>We have received a payment of Rs. " . $amount . ". Your order will soon be shipped.</h4>";
-           
-		   }         
-?>	
+	<link href="font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="scripts/jquery-ui-1.11.2/jquery-ui.min.css" rel="stylesheet" />
+	<link href="animate.min.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+	<script>
+		function initialize() {
+			var myLatlng = new google.maps.LatLng(12.894655,77.675153);
+			var map_options = {
+				zoom: 16,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				center: myLatlng,
+				scrollwheel: false,
+				disableDefaultUI: true
+			}
+
+			var map = new google.maps.Map(document.getElementById('map_canvas'), map_options)
+			var myIcon = new google.maps.MarkerImage('images/map_icon.png', null, null, null, new google.maps.Size(36,54));
+
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				icon: myIcon
+			});
+
+			/* Remove if you dont want B/W Google Map */
+			/*
+			 var styles = [
+			 {
+			 featureType: "all",
+			 stylers: [
+			 { saturation: -100 }
+			 ]
+			 }
+			 ];
+
+			 map.setOptions({styles: styles});
+			 */
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
+	<!-- Google Map Ends! -->
+
+</head>
+
+<body>
+
+<div class="registration" style="padding-top:5%">
+	<div class="container">
+
+		<h2>Conference Registration</h2>
+		<p class="subtitle"><strong>We have your transaction.</strong> We  will be in touch with you soon :)</p>
+
+
+		<p class="desc" style="padding-top: 10%">
+			Your transaction id is: <?php echo $_POST['txnid']; ?> <br>
+			The transacted amount is : <?php echo $_POST['amount']; ?> <br>
+		</p>
+
+	</div>
+	<!-- registration ends! -->
+
+	<!-- footer -->
+	<div class="footer" style=" bottom: 0px; position: fixed; width: 100%;">
+		<div class="container" style="text-align: center">
+
+			<p style="text-align: center">Copyright 2016 <a href="http://www.amrita.edu">Amrita Center for Cybersecurity Systems and Networks</a></p>
+
+		</div>
+	</div>
+	<!-- footer ends! -->
+
+
+	<!-- scripts -->
+	<script src="scripts/jquery-1.11.0.min.js"></script>
+	<script src="scripts/jquery-ui-1.11.2/jquery-ui.min.js"></script>
+	<script src="scripts/flexslider/jquery.flexslider-min.js"></script>
+	<script src="scripts/jquery.parallax-1.1.3.js"></script>
+	<script src="scripts/jquery.inview.min.js"></script>
+	<script src="scripts/form.js"></script>
+	<script src="scripts/theme.js"></script>
+	<!-- scripts ends! -->
+</body>
+</html>
